3798633318356ed270dba8fd996f0d305a718f79
hjm12223 <gwa5068@gmail.com>
2023-11-05 18:37:22 +0900

    feat: 로그아웃 기능 추가

--- src/main/java/HIM/project/controller/TokenController.java
+++ src/main/java/HIM/project/controller/TokenController.java
@@ -1,15 +1,14 @@
 package HIM.project.controller;
 
 
+import HIM.project.common.ErrorCode;
 import HIM.project.common.ResponseDto;
+import HIM.project.service.RedisService;
 import HIM.project.service.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -20,9 +19,18 @@ import javax.servlet.http.HttpServletResponse;
 public class TokenController {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final RedisService redisService;
 
-    @GetMapping("refreshtoken")
+
+    @GetMapping("/reissuance")
     public ResponseEntity<?> refreshToken(@RequestHeader(name = "Authorization") String accessToken , HttpServletResponse response){
         return jwtTokenProvider.reissuance(accessToken, response);
+    }
+
+    @DeleteMapping("/logout")
+    public ResponseDto<?> deleteRefreshToken(@RequestHeader(name = "Authorization") String accessToken) {
+        Long userId = jwtTokenProvider.getUserId(accessToken);
+        redisService.deleteDictionary(String.valueOf(userId));
+        return ResponseDto.success("성공적으로 제거하였습니다");
     }
 }
