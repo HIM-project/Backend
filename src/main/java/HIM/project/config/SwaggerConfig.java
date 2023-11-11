ba6b80d6087d68b241cf2ee3f1ca33d00dbb853f
hjm12223 <gwa5068@gmail.com>
2023-11-11 19:43:22 +0900

    feat: 스웨거 및 S3 추가

---
+++ src/main/java/HIM/project/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package HIM.project.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "HIM",
+                description = "API 명세서",
+                version = "v1.0.0"
+        )
+)
+@SecuritySchemes({
+        @SecurityScheme(
+                type = SecuritySchemeType.HTTP,
+                name = "JWT 토큰 인증",
+                description = "로그인할 때 받았던 JWT 토큰을 기입해주세요.",
+                in = SecuritySchemeIn.HEADER,
+                paramName = HttpHeaders.AUTHORIZATION,
+                scheme = "Bearer",
+                bearerFormat = "JWT"
+        )
+})
+public class SwaggerConfig {}
---
+++ src/main/java/HIM/project/service/S3Service.java
@@ -0,0 +1,92 @@
+package HIM.project.service;
+
+import HIM.project.common.ErrorCode;
+import HIM.project.exception.CustomException;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.Set;
+
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class S3Service {
+
+    private final AmazonS3Client amazonS3Client;
+    private final Set<String> ALLOWED_IMAGE_EXTENSIONS = Set.of(".png", ".jpg", ".jpeg");
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private void validateFileExtension(String fileName) {
+        try {
+            String extension = fileName.substring(fileName.lastIndexOf(".")).toLowerCase();
+            if (validateImageExtension(extension)) return;
+
+            throw new CustomException(ErrorCode.IMAGE_EXTENSION_BAD_REQUEST);
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new CustomException(ErrorCode.IMAGE_INVALID_FORMAT);
+        }
+    }
+    public void deleteFile(String fileUrl) {
+        try {
+            URL url = new URL(fileUrl);
+            String path = url.getPath();
+            String filename = path.substring(path.lastIndexOf('/') + 1);
+
+            amazonS3Client.deleteObject(bucket, filename);
+
+        } catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public String uploadImageFile(MultipartFile file) throws IOException {
+        if (!validateImageFile(file)) throw new CustomException(ErrorCode.IMAGE_INVALID_FORMAT);
+        return upload(file);
+    }
+
+    private boolean validateImageFile(MultipartFile file) {
+        return validateImageExtension(parseExtension(file));
+    }
+
+    private boolean validateImageFiles(Collection<MultipartFile> files) {
+        Objects.requireNonNull(files);
+
+        return files.stream()
+                .map(this::parseExtension)
+                .allMatch(this::validateImageExtension);
+    }
+
+    private String parseExtension(MultipartFile file) {
+        String fileName = Objects.requireNonNull(file.getOriginalFilename());
+        return fileName.substring(fileName.lastIndexOf(".")).toLowerCase();
+    }
+
+    private Boolean validateImageExtension(String extension) {
+        return ALLOWED_IMAGE_EXTENSIONS.contains(extension);
+    }
+
+    private String upload(MultipartFile file) throws IOException {
+        ObjectMetadata meta = new ObjectMetadata();
+        meta.setContentType(file.getContentType());
+        meta.setContentLength(file.getSize());
+
+        String fileName = file.getOriginalFilename();
+
+        amazonS3Client.putObject(bucket, fileName, file.getInputStream(), meta);
+
+        return amazonS3Client.getUrl(bucket, fileName).toString();
+    }
+}
