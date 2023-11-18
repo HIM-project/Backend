699abf1506c001788829e18a5b97dc92dc966b71
hjm12223 <gwa5068@gmail.com>
2023-11-18 17:37:02 +0900

    chore: 어노테이션 및 에러코드 수정

--- src/main/java/HIM/project/controller/MenuController.java
+++ src/main/java/HIM/project/controller/MenuController.java
@@ -5,6 +5,7 @@ import HIM.project.common.ResponseDto;
 import HIM.project.dto.MenuDto;
 import HIM.project.security.argumentreoslver.AuthUser;
 import HIM.project.service.MenuService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -17,8 +18,10 @@ import java.util.List;
 @Slf4j
 @RequestMapping("/api/v1")
 public class MenuController {
+
     private final MenuService menuService;
     @PostMapping("register/menu")
+    @Operation(summary = "메뉴 등록")
     public ResponseDto<?> registrationMenu(@AuthUser Long userId, @RequestPart(name = "key") List<MenuDto> menuDto, @RequestPart(name = "file")List<MultipartFile> file){
         return menuService.uploadMenu(menuDto,userId,file);
     }
