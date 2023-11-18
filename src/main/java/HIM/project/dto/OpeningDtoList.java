49045d20b08f1b1054282f850f0868d1f40186e2
hjm12223 <gwa5068@gmail.com>
2023-11-18 17:38:08 +0900

    feat: 영업시간 등록 기능 및 Dto, Entity, Repo 추가

---
+++ src/main/java/HIM/project/dto/OpeningDtoList.java
@@ -0,0 +1,30 @@
+package HIM.project.dto;
+
+import HIM.project.entity.OpeningTime;
+import HIM.project.entity.Restaurant;
+import HIM.project.entity.type.Day;
+import lombok.*;
+
+import java.sql.Time;
+import java.util.List;
+
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class OpeningDtoList {
+    private List<OpeningDto> openingDtoList;
+
+    @ToString
+    @Getter
+    public static class OpeningDto {
+        private Long restaurantId;
+        private Day day;
+        private Time open;
+        private Time close;
+    }
+
+}
+
--- src/main/java/HIM/project/respository/RestaurantRepository.java
+++ src/main/java/HIM/project/respository/RestaurantRepository.java
@@ -6,11 +6,16 @@ import HIM.project.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant,String> {
-    Optional<Restaurant> findAllByCrNumber(String crNumber);
+    Boolean existsAllByCrNumber(String crNumber);
+
+    Optional<List<Restaurant>> findAllByUserUserId(Long userId);
+
+    Optional<Restaurant> findAllByRestaurantId(Long restaurantId);
 
     Optional<Restaurant> findAllByUser(User user);
 }
