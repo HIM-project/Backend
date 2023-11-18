aaac3bffa540f3581ed96fc273217a379d39e852
hjm12223 <gwa5068@gmail.com>
2023-11-18 17:36:30 +0900

    chore: 어노테이션 추가

--- src/main/java/HIM/project/respository/RefreshTokenRepository.java
+++ src/main/java/HIM/project/respository/RefreshTokenRepository.java
@@ -2,7 +2,9 @@ package HIM.project.respository;
 
 import HIM.project.entity.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
     RefreshToken findByUserId(String userId);
 
